2a2f1e302fc3cb3979a4436771663bf2f20d315e
Ahmed <shiehaa53@gmail.com>
2021-04-24 15:16:18 +0200

    add account&record

---
+++ SAVIT/Model/Record.swift
@@ -0,0 +1,9 @@
+//
+//  Record.swift
+//  SAVIT
+//
+//  Created by Reda on 23/03/2021.
+//  Copyright © 2021 Reda. All rights reserved.
+//
+
+import Foundation
---
+++ SAVIT/TabBar/DashBoradStoryBoards/DashBoardViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DashBoardViewController.swift
+//  SAVIT
+//
+//  Created by Reda on 23/03/2021.
+//  Copyright © 2021 Reda. All rights reserved.
+//
+
+import UIKit
+
+class DashBoardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
